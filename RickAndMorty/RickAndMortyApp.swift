537e545ab2142b04b94268506b3b5cd8422e97b2
Abhishek Tyagi <abhishek.tyagi@mail.vzwdt.com>
2022-02-01 15:09:56 +0530

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Abhishek Tyagi on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
